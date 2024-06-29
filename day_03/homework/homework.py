4d3ebce859cc9bb3032f93cd8bd85653d8d7c64b
Nikushatavartkiladze <nikatavartkiladze09@icloud.com>
2024-06-30 00:30:02 +0400

    third

---
+++ day_03/homework/homework.py
@@ -0,0 +1,11 @@
+car = "BMW "
+name = "F30 "
+production  = "2011-2019 "
+manufacture = "Germany "
+price = "9000 $ "
+lenght = "4624 millimeter "
+width = "1811 millimeter "
+height = "1429 millimeter "
+
+
+print(car + name + production + manufacture + price + lenght + width + height)
