7335e0fb4b2261c620272496cc87a415b855ae90
Nikushatavartkiladze <nikatavartkiladze09@icloud.com>
2024-06-30 01:01:21 +0400

    second commit

---
+++ day_03/homework/homework.py
@@ -0,0 +1,8 @@
+car = "BMW "
+name = "F30 "
+production = "2011-2019 "
+manufacturer = "Germany "
+height = "1429 millimeter "
+width = "1811 millimeter "
+lenght = "4624 millimeter "
+print(car + name + production + manufacturer + height + width + lenght)
