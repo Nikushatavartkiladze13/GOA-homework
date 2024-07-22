52875af2a4b529500357f5e706cbac17feb06173
Nikushatavartkiladze <nikatavartkiladze09@icloud.com>
2024-07-22 15:49:31 +0400

    9th commit

---
+++ day_09/homework/homework.py
@@ -0,0 +1,10 @@
+# 1) sololearn module 3 quiz ის ჩათვლით
+ 
+
+#  2) შექმენით დროში მოგზაურობის პროგრამა, რომელიც მომხმარებელს შეეკითხება მის ამჟამინდელ ასაკს, ამჟამინდელ წელს, რამდენი წლით სურს დროში მოგზაურობა, შემდეგ კი პროგრამა დაბეჭდავს დროში მოგზაურობის შემდეგ რომელი წელი იქნება და რამდენი წლის იქნება მომხმარებელი დროში მოგზაურობის შემდეგ
+
+input("how old are you?: ")
+input("what year is it?: ")
+input("How many years do you want to travel back in time?: ")
+
+print("After time travel from 2024, they will be 26 years old")
