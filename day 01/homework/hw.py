64523d4845428fbfb4b8dae694aec14b95eb1eae
Nikushatavartkiladze13 <nikatavartkiladze09@icloud.com>
2025-02-27 22:52:20 +0400

    first commit

---
+++ day 01/homework/hw.py
@@ -0,0 +1,9 @@
+name = "nika" #string 
+surname = "Tavartkiladze"
+age = 15 # integer 
+height = 1.82# float
+lives = "khulo"
+hobby = "kalatburtis tamashi"
+
+
+print("me var " + name + " " + surname + " var " + str(age) + " wlis. " + " chemi simagl aris " + str(height) + " me vcxovrob " + lives + "  chemi hobi aris " + hobby)
