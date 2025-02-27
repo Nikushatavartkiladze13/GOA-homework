42edef84522dd525f60b0ea1adfe1f7f04b23f5d
Nikushatavartkiladze13 <nikatavartkiladze09@icloud.com>
2025-02-28 02:14:24 +0400

    second commit

---
+++ day 10/classwork/cw.py
@@ -0,0 +1,9 @@
+# /1 ) მომხმარებელს შემოატანინეთ რიცხვი შემდეგ კი დაბეჭდეთ ეს რიცხვი არის თუ არა სამის ჯერადი
+
+number = int(input("enter your number: "))
+
+
+if number % 3 == 0:
+    print(True)
+else:
+    print(False)
