5323794649883b629d2dc721716a61f562fcf414
nikushatavartkiladze13 <nikatavartkiladze09@icloud.com>
2025-04-17 21:25:46 +0400

    first commit

---
+++ day_01/homework/hw.py
@@ -0,0 +1,14 @@
+name="nika tavartkiladze"
+surname = "tavartkiladze"
+
+
+
+print(name)
+birth = "2009"
+name = "nika"         # (string-)
+age = 14                  #(integer)
+height = 175,4            #float ტიპის (ათწილადი)
+life = "khuloshi"
+knows_programming = True   #true or folse
+
+print("me var nika tavartkiladze" + " " + str(age) + " "  +  "wlis" + " " + "me davibade" + " " + birth +" " + "wles"+ " " + "me simagleshi var" + " " + str(height) + " " + "vcxovrob" + " " + life)
