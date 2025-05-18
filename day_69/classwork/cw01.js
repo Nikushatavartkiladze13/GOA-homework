cca2efbdae9a4e55bb974d904d76c7d2cbf46d30
nikushatavartkiladze13 <nikatavartkiladze09@icloud.com>
2025-05-18 21:33:25 +0400

    second commit:

--- day_69/classwork/cw01.js
+++ day_69/classwork/cw01.js
@@ -9,23 +9,23 @@ let i = 1
 
 
 while(i < userNumber){
-    if(i % 2 == 0 && userChoice == "luwi"){
+    if(i % 2==0  &&  userChoice == "luwi"  ){
         console.log(i)
-    }
-    else if(i % 2 == 1 && userChoice == "kenti"){
+    }else if(i % 2==1 && userChoice == "kenti"){
         console.log(i)
-    }
-    else if(userChoice == "kvadrati"){
+    }else if(userChoice=="kvadrati"){
         console.log(i ** 2)
 
-    }
-    else if(i % 3 == 0 && userChoice == "samis jeradi"){
+    }else if(i % 3 == 0 && userChoice == "samisjeradi"){
         console.log(i)
-    }
-    else{
+    }else{
         break
     }
-i++
+
+i++ 
+
+
+
 }
 
 
