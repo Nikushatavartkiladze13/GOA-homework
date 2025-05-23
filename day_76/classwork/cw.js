f766e8e14a0d3ed32bc41da10a7502822480de81
nikushatavartkiladze13 <nikatavartkiladze09@icloud.com>
2025-05-23 17:22:20 +0400

    second commit

---
+++ day_76/classwork/cw.js
@@ -0,0 +1,28 @@
+// 1)შექმენით რამდენიმე თეგი HTML-ში და javascript-ის გამოყენებით მიეცით სასურველი სტილები და შეუცვალეთ კონტენტი
+
+
+
+let heading = document.getElementsByTagName("h1")
+let button = document.getElementsByTagName("button")
+
+heading[0].innerHTML = "Nika Tavartkiladze"
+heading[0].style.color = "red"
+button[0].style.width = "200px"
+button[0].style.height = "40px"
+button[0].style.backgroundColor = "green"
+button[0].style.color = "white"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ day_76/classwork/cw01.js
@@ -0,0 +1,9 @@
+// 2)შექმენით ფუნქცია რომელიც შეცვლის ელემენტის შიგთავსს , გამოიყენეთ onClick - მეთოდი 
+
+let h1 = document.getElementsByTagName("h1")
+
+function changeName(){
+    h1[0].innerHTML = "Nika Tavartkiladze"
+}
+
+
